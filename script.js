d77df1352763478f2b79006b68318066830ceb35
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-11-10 23:31:35 +0530

    toggle menu
    Signed-off-by: Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const hideItemsNavbar =document.getElementsByClassName("block-nav-menu")[0];
 
 const navToggle  = document.getElementById("nav-toggle");
 
 navToggle.addEventListener("click",()=>{
-    console.log(hideItemsNavbar.style.display)
-    if( hideItemsNavbar.style.display==="none"){
-        hideItemsNavbar.style.display = "block"
-    }else if(hideItemsNavbar.style.display==="block"){
-        hideItemsNavbar.style.display ="none"
+    const hideItemsNavbar =document.getElementsByClassName("block-nav-menu")[0];
+    console.log(window.getComputedStyle(hideItemsNavbar).display)
+    if( window.getComputedStyle(hideItemsNavbar).display==="none"){
+        hideItemsNavbar.style.display="block"
+    }else {
+        hideItemsNavbar.style.display="none"
     }
 })
 

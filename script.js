63892dbcadf23890869bd0fd13dc843fce05372c
Pawan123Pawan <pawanmaurya4277@gmail.com>
2023-11-10 22:20:42 +0530

    header created #3 Signed-off-by: Pawan123Pawan <pawanmaurya4277@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert{"hii"}
+const hideItemsNavbar =document.getElementsByClassName("block-nav-menu")[0];
+
+const navToggle  = document.getElementById("nav-toggle");
+
+navToggle.addEventListener("click",()=>{
+    console.log(hideItemsNavbar.style.display)
+    if( hideItemsNavbar.style.display==="none"){
+        hideItemsNavbar.style.display = "block"
+    }else if(hideItemsNavbar.style.display==="block"){
+        hideItemsNavbar.style.display ="none"
+    }
+})
+
